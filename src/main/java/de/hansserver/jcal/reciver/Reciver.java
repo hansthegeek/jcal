64065d8a3bdce55e3df261c345cf9301ecaf6ca2
raphael <raphael@MacBookPro>
2014-01-31 23:56:17 +0100

    Reciver and Sender base classes added

---
+++ src/main/java/de/hansserver/jcal/reciver/Reciver.java
@@ -0,0 +1,13 @@
+/*
+ * created: 31.01.2014
+ */
+
+package de.hansserver.jcal.reciver;
+
+/**
+ *
+ * @author Raphael Esterle
+ */
+public abstract class Reciver {
+    
+}
---
+++ src/main/java/de/hansserver/jcal/sender/Sender.java
@@ -0,0 +1,13 @@
+/*
+ * created: 31.01.2014
+ */
+
+package de.hansserver.jcal.sender;
+
+/**
+ *
+ * @author Raphael Esterle
+ */
+public class Sender {
+    
+}
